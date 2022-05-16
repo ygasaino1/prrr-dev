2cc51c3dae7cf2483a4b9ce16505e2843dd6deb6
Tiki <54782797+ygasaino1@users.noreply.github.com>
2022-05-17 03:31:58 +0430

    ...

--- player_script.js
+++ player_script.js
@@ -1,28 +1,31 @@
-var tag = document.createElement('script');
-tag.id = 'iframe-demo';
-tag.src = 'https://www.youtube.com/iframe_api';
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-var player;
-
-function onYouTubeIframeAPIReady() {
-    // ?open=https://www.youtube.com/embed/QEPYBkO2mX0
-    let search = window.location.search.split('open=');
-    let elem = document.querySelector('#player');
-    if (search.length < 2 || search[2] == '' || elem == undefined) { return; }
-
-    elem.setAttribute(src, elem[1]);
-    player = new YT.Player(elem, {
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        }
-    });
+let elem = document.querySelector('#player');
+let search = window.location.search.split('open=');
+if (search.length == 2 && search[1] != '') {
+    elem.setAttribute('src', search[1]);
 }
 
-function onPlayerReady(event) {
-    player.playVideo();
-}
+if (elem.getAttribute('src') != "no-source") {
+    let tag = document.createElement('script');
+    tag.id = 'iframe-demo';
+    tag.src = 'https://www.youtube.com/iframe_api';
+    let firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-function onPlayerStateChange(event) {}
+    var player;
+
+    function onYouTubeIframeAPIReady() {
+        // ?open=https://www.youtube.com/embed/QEPYBkO2mX0
+        player = new YT.Player('player', {
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            }
+        });
+    }
+
+    function onPlayerReady(event) {
+        player.playVideo();
+    }
+
+    function onPlayerStateChange(event) {}
+}
