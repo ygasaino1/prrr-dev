d860dea8d0f31a01850d5d8d417d030713a1bd82
Tiki <54782797+ygasaino1@users.noreply.github.com>
2022-05-18 00:20:45 +0430

    ...

--- public/init.js
+++ public/init.js
@@ -3,7 +3,7 @@ let player_elem = document.querySelector('#player');
 let player_audio = document.querySelector('#player_audio');
 let canvas = document.querySelector('canvas');
 
-let visual = function() {
+let visual_ = function() {
     var audioCtx = new(window.AudioContext || window.webkitAudioContext)();
     var analyser = audioCtx.createAnalyser();
 
@@ -14,7 +14,7 @@ let visual = function() {
     // distortion.connect(audioCtx.destination);
 
     analyser.fftSize = 64;
-    var bufferLength = analyser.frequencyBinCount;
+    var bufferLength = analyser.frequencyBinCount; //
     var dataArray = new Uint8Array(bufferLength);
 
     let canvasCtx = canvas.getContext('2d');
@@ -44,6 +44,77 @@ let visual = function() {
             canvasCtx.fillRect(WIDTH - barHeight, y, barHeight, barWidth);
             y += (barWidth + gap);
         }
+    };
+
+    draw();
+}
+
+let dataArray;
+// let min = -130;
+// let max = -80;
+// let smooth = 0.5;
+// let vis_kind = 0;
+
+let max = -60;
+let min = -61;
+let offset = 0;
+let smooth = 0.9;
+let vis_kind = 0;
+
+let visual = function() {
+    var audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+    var analyser = audioCtx.createAnalyser();
+
+
+    let source = audioCtx.createMediaElementSource(player_audio);
+    source.connect(analyser);
+    // source.connect(audioCtx.destination);
+    // analyser.connect(distortion);
+    // distortion.connect(audioCtx.destination);
+
+    analyser.fftSize = 32;
+    var bufferLength = analyser.frequencyBinCount; //analyser.frequencyBinCount
+    dataArray = new Uint8Array(bufferLength);
+
+    let canvasCtx = canvas.getContext('2d');
+    canvas.width = 255;
+    canvas.height = window.innerHeight;
+    let HEIGHT = canvas.height;
+    let WIDTH = canvas.width;
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+    function draw() {
+        if ((max + offset) < analyser.minDecibels) {
+            analyser.minDecibels = max + offset - 1;
+        }
+        analyser.maxDecibels = max + offset; //-30
+        analyser.minDecibels = min + offset; //-100
+        analyser.smoothingTimeConstant = smooth; //0.8
+        drawVisual = requestAnimationFrame(draw);
+        if (vis_kind == 0) {
+            analyser.getByteFrequencyData(dataArray);
+        } else if (vis_kind == 1) {
+            analyser.getByteTimeDomainData(dataArray);
+        }
+
+
+        canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+        canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+        var y = 0;
+        let gap = 5
+        var barWidth = ((HEIGHT - (bufferLength - 1) * gap) / bufferLength);
+        var barHeight;
+
+        for (var i = 0; i < bufferLength; i++) {
+            barHeight = dataArray[i];
+
+            canvasCtx.fillStyle = 'rgb(255, 255, 255)';
+            canvasCtx.fillRect(WIDTH - barHeight, y, barHeight, barWidth);
+            y += (barWidth + gap);
+        }
+        // canvasCtx.fillStyle = 'rgb(255, 255, 255)';
+        // canvasCtx.fillRect(WIDTH - dataArray[0], y, dataArray[0], HEIGHT);
     };
 
     draw();
--- public/player_script.js
+++ public/player_script.js
@@ -1,11 +1,30 @@
 let search = window.location.search.split('open=');
-// ?open=https://www.youtube.com/embed/QEPYBkO2mX0
+// https://www.youtube.com/embed/QEPYBkO2mX0?playlist=${}&enablejsapi=1&controls=0&loop=1
+// https://www.youtube.com/embed/videoseries?list=${}&enablejsapi=1&controls=0&loop=1
 if (search.length == 2 && search[1] != '') {
-    let url_ = new URL(search[1]);
+    let url_;
+    if (search[1].toLocaleLowerCase().includes('list') && !search[1].includes('list=LL')) {
+        url_ = new URL(search[1]);
+        let list_id_ = '';
+        if (url_.searchParams.has('playlist')) { list_id_ = url_.searchParams.get('playlist') }
+        if (url_.searchParams.has('list')) { list_id_ = url_.searchParams.get('list') }
+        url_ = `https://www.youtube.com/embed/videoseries?list=${list_id_}&enablejsapi=1&controls=0&loop=1`;
+    } else {
+        let id_ = YouTubeGetID(search[1]);
+        url_ = `https://www.youtube.com/embed/${id_}?playlist=${id_}&enablejsapi=1&controls=0&loop=1`;
+    }
     // if (!url_.searchParams.has('autoplay')) { url_.searchParams.set('autoplay', 1) }
-    if (!url_.searchParams.has('controls')) { url_.searchParams.set('controls', 0) }
-    if (!url_.searchParams.has('enablejsapi')) { url_.searchParams.set('enablejsapi', 1) } //
-    player_elem.setAttribute('src', url_.href);
+    // if (!url_.searchParams.has('loop')) { url_.searchParams.set('loop', 1) }
+    // if (!url_.searchParams.has('controls')) { url_.searchParams.set('controls', 0) }
+    // if (!url_.searchParams.has('enablejsapi')) { url_.searchParams.set('enablejsapi', 1) }
+    // if (!url_.searchParams.has('playlist')) { url_.searchParams.set('playlist', id_) }
+    console.log(url_);
+    player_elem.setAttribute('src', url_);
+}
+
+function YouTubeGetID(v) {
+    v = v.split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+    return (v[2] !== undefined) ? v[2].split(/[^0-9a-z_\-]/i)[0] : v[0];
 }
 
 if (player_elem.getAttribute('src') != "no-source") {
@@ -28,30 +47,31 @@ if (player_elem.getAttribute('src') != "no-source") {
     }
 
     function onPlayerReady(event) {
-        play();
+        player.playVideo();
     }
     let ended = false;
 
     function onPlayerStateChange(event) {
-        if (event.data == 0) {
-            ended = true; // ended = yellow
-            console.log('video:end;');
-        } else if (event.data == 1 && ended == true) {
-            ended = false; // playing = green
-            play();
-            console.log('video:replay;');
-        } else if (event.data == 1) {
-            player_audio.play();
-            console.log('video:resume;');
+        if (event.data == 1) {
+            console.log('1');
+            if (ended) {
+                ended = false;
+                audio_sync();
+            }
         } else if (event.data == 2) {
-            player_audio.pause();
-            console.log('video:pause;');
+            console.log('2');
+        } else if (event.data == 3) {
+            console.log('3');
+        } else if (event.data == -1) {
+            console.log('-1');
+            ended = true;
+        } else if (event.data == 5) {
+            console.log('5');
         }
     }
 
-    function play() {
+    function audio_sync() {
         let video_id = player.getVideoData()['video_id'];
-        // let audio_url = `https://prrr-001.glitch.me/ytid/${video_id}`;
         let audio_url = `https://prrr-dev.glitch.me/ytid/${video_id}`;
         player_audio.setAttribute('src', audio_url);
         player_elem.style.visibility = 'hidden';
