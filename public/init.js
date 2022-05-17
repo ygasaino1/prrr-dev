a0cb147fafd289c94d201ac9e5ed8e03fc268c8b
Tiki <54782797+ygasaino1@users.noreply.github.com>
2022-05-18 01:25:41 +0430

    .............

--- public/init.js
+++ public/init.js
@@ -55,11 +55,17 @@ let dataArray;
 // let smooth = 0.5;
 // let vis_kind = 0;
 
-let max = -60;
-let min = -61;
-let offset = 0;
+// let max = -60;
+// let min = -61;
+// let smooth = 0.9;
+// let vis_kind = 0;
+
+let max = -30;
+let min = -70;
 let smooth = 0.9;
 let vis_kind = 0;
+
+let offset = 0;
 
 let visual = function() {
     var audioCtx = new(window.AudioContext || window.webkitAudioContext)();
@@ -68,12 +74,10 @@ let visual = function() {
 
     let source = audioCtx.createMediaElementSource(player_audio);
     source.connect(analyser);
-    // source.connect(audioCtx.destination);
-    // analyser.connect(distortion);
-    // distortion.connect(audioCtx.destination);
 
     analyser.fftSize = 32;
     var bufferLength = analyser.frequencyBinCount; //analyser.frequencyBinCount
+    console.log(bufferLength);
     dataArray = new Uint8Array(bufferLength);
 
     let canvasCtx = canvas.getContext('2d');
@@ -103,18 +107,28 @@ let visual = function() {
 
         var y = 0;
         let gap = 5
-        var barWidth = ((HEIGHT - (bufferLength - 1) * gap) / bufferLength);
+        var barWidth = (HEIGHT - (bufferLength * gap)) / (bufferLength + 1);
         var barHeight;
 
+        canvasCtx.fillStyle = 'rgb(255, 255, 255)';
+        canvasCtx.fillRect(0, y, WIDTH, barWidth);
+        y += (barWidth + gap);
+
         for (var i = 0; i < bufferLength; i++) {
-            barHeight = dataArray[i];
+            if (dataArray[i] == 0) {
+                barHeight = 0;
+            } else if (dataArray[i] > 127) {
+                barHeight = 255;
+            } else {
+                barHeight = 127;
+            }
 
             canvasCtx.fillStyle = 'rgb(255, 255, 255)';
             canvasCtx.fillRect(WIDTH - barHeight, y, barHeight, barWidth);
             y += (barWidth + gap);
         }
-        // canvasCtx.fillStyle = 'rgb(255, 255, 255)';
-        // canvasCtx.fillRect(WIDTH - dataArray[0], y, dataArray[0], HEIGHT);
+        canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+        canvasCtx.fillRect((WIDTH / 2) - (gap), 0, (gap * 2), HEIGHT);
     };
 
     draw();
